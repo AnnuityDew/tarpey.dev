ae2198521a60ab6cefc5748a9e4510caf3459c87
Michael Tarpey <miketarpey@MacBook-Pro.local>
2020-06-24 22:32:51 -0400

    Added DDR save files as of June 24 2020

--- tarpeydev/ddr.py
+++ tarpeydev/ddr.py
@@ -5,12 +5,6 @@ import base64
 
 # import third party packages
 from flask import Blueprint, render_template, request
-from matplotlib import cm, rcParams
-from matplotlib.colors import Normalize
-from matplotlib.figure import Figure
-import numpy
-import pandas
-import seaborn
 
 
 ddr_bp = Blueprint('ddr', __name__, url_prefix='/ddr')
@@ -21,4 +15,40 @@ ddr_bp = Blueprint('ddr', __name__, url_prefix='/ddr')
 def home():
     return render_template(
         'ddr/home.html',
+    )
+
+
+def which_save_file(lookup):
+    save_folder_path = os.path.join(
+        os.getcwd(),
+        'data',
+        'ddr',
+    )
+    
+    save_file_dictionary = {
+        'max_jp': os.path.join(save_folder_path, 'BISLPM-62154system'),
+        'max2_jp': os.path.join(save_folder_path, 'BISLPM-65277system'),
+        'ex_jp': os.path.join(save_folder_path, 'BISLPM-65358system'),
+        'max_us': os.path.join(save_folder_path, 'BASLUS-20437system'),
+        'max2_us': os.path.join(save_folder_path, 'BASLUS-20711system'),
+        'ex_us': os.path.join(save_folder_path, 'BASLUS-20916system'),
+        'ex2_us': os.path.join(save_folder_path, 'BASLUS-21174system'),
+        'sn_us': os.path.join(save_folder_path, 'BASLUS-21377system'),
+        'sn2_us': os.path.join(save_folder_path, 'BASLUS-21608system'),
+        'x_us': os.path.join(save_folder_path, 'BASLUS-21767system'),
+    }
+
+    return save_file_dictionary[lookup]
+
+
+def read_max2_us_save():
+    with open(which_save_file('max2_us'), 'rb') as max2_us:
+        hex_list = ["{:02x}".format(ord(c)) for c in fp.read()]
+        test = max2_us.read()
+
+
+def read_max_us_save():
+    save_file_path = os.path.join(
+        os.getcwd(),
+        'data'
     )
