ae738b8397d0a235ebcbb39247077b032ef89f8b
AnnuityDew <annuitydew@gmail.com>
2020-11-22 19:35:55 -0500

    Cleaned up hysx frontpage

--- tarpeydev/haveyouseenx.py
+++ tarpeydev/haveyouseenx.py
@@ -1,6 +1,5 @@
 # import native Python packages
 import json
-import os
 
 # import third party packages
 from flask import Blueprint, render_template, request
@@ -11,7 +10,6 @@ import plotly.express as px
 
 # import local stuff
 from tarpeydev import api
-from tarpeydev.users import login_required
 
 
 hysx_bp = Blueprint('haveyouseenx', __name__, url_prefix='/haveyouseenx')
@@ -32,10 +30,13 @@ def home():
         playtime[0].get('total_hours')
     )
 
-    # create visualizations
+    # create visualizations.
+    # pass copies to the function because they each
+    # do a different manipulation
     backlog_data, response_code = api.backlog()
     backlog_df = pandas.DataFrame(backlog_data.json)
-    treemap = system_treemap(backlog_df)
+    treemap = system_treemap(backlog_df.copy())
+    
     (
         x_data_counts,
         y_data_dist,
@@ -43,7 +44,11 @@ def home():
         bubble_names,
         label_text,
         color_data
-    ) = system_bubbles(backlog_df)
+    ) = system_bubbles(backlog_df.copy())
+
+    (
+        x_data_c, x_data_b, x_data_s, x_data_ns, y_data_dates, area_colors
+    ) = timeline(backlog_df.copy(), stats)
 
     return render_template(
         'haveyouseenx/home.html',
@@ -56,6 +61,12 @@ def home():
         bubble_names=bubble_names,
         label_text=label_text,
         color_data=color_data,
+        x_data_c=x_data_c,
+        x_data_b=x_data_b,
+        x_data_s=x_data_s,
+        x_data_ns=x_data_ns,
+        y_data_dates=y_data_dates,
+        area_colors=area_colors,
     )
 
 
@@ -68,24 +79,6 @@ def results():
         search_term=request.args.get('query'),
         results=results,
     )
-
-
-def read_backlog():
-    # file path
-    backlog_path = os.path.join(
-        os.getcwd(),
-        'data',
-        'haveyouseenx',
-        'haveyouseenx_annuitydew.csv'
-    )
-    # read backlog
-    backlog = pandas.read_csv(
-        backlog_path,
-        index_col='game_id',
-        encoding='latin1'
-    )
-
-    return backlog
 
 
 def system_treemap(backlog):
@@ -208,3 +201,116 @@ def system_bubbles(backlog):
     return (
         x_data_counts, y_data_dist, z_data_hours, bubble_names, label_data, color_data
     )
+
+
+def timeline(backlog, stats):
+    # drop unused columns, move dates to x axis to create timeline
+    # sort for most recent event at the top
+    backlog = backlog.drop(
+        columns=[
+            'game_hours',
+            'game_minutes',
+            'game_notes',
+            'game_status',
+            'game_system',
+            'genre',
+            'now_playing',
+            'playtime_calc',
+            ]
+        ).melt(
+            id_vars=['_id', 'game_title', 'sub_title'],
+            var_name='event_name',
+            value_name='event_date',
+        )
+
+    # fill empty cells with the backlog's birth date
+    backlog['event_date'] = (
+        backlog['event_date'].fillna(numpy.datetime64('2011-10-08T16:00:00'))
+    )
+
+    # event date to datetime
+    backlog.event_date = pandas.to_datetime(backlog.event_date, utc=True)
+
+    # sort by date descending
+    backlog.sort_values(['event_date'], ascending=False, inplace=True)
+
+    # reset index
+    backlog.reset_index(inplace=True)
+
+    # next, place current status counts in the first row.
+    # then we'll be able to calculate the backlog at older
+    # points in time using the timeline
+    backlog['not_started'] = stats.get('Not Started')
+    backlog['started'] = stats.get('Started')
+    backlog['beaten'] = stats.get('Beaten')
+    backlog['completed'] = stats.get('Completed')
+    backlog = backlog.assign(ns=0, s=0, b=0, c=0)
+
+    # initalize modifiers
+    mod_ns, mod_s, mod_b, mod_c = 0, 0, 0, 0
+
+    for row in backlog.itertuples():
+        backlog.at[row.Index, 'ns'] += mod_ns
+        backlog.at[row.Index, 's'] += mod_s
+        backlog.at[row.Index, 'b'] += mod_b
+        backlog.at[row.Index, 'c'] += mod_c
+        if row.event_name == 'add_date':
+            mod_ns += 1
+        elif row.event_name == 'start_date':
+            mod_ns -= 1
+            mod_s += 1
+        elif row.event_name == 'beat_date':
+            mod_s -= 1
+            mod_b += 1
+        elif row.event_name == 'complete_date':
+            mod_b -= 1
+            mod_c += 1
+
+    # now recalculate the timeline values. this is our final data
+    backlog['ns'] = backlog['not_started'] - backlog['ns']
+    backlog['s'] = backlog['started'] - backlog['s']
+    backlog['b'] = backlog['beaten'] - backlog['b']
+    backlog['c'] = backlog['completed'] - backlog['c']
+
+    # change sort to ascending and drop unnecessary columns
+    # set index to event date
+    backlog = backlog.sort_values(
+        ['event_date'], ascending=True
+    )[['event_date', 'ns', 's', 'b', 'c']]
+
+    # drop duplicate dates (keep last, that will be most recent)
+    backlog.drop_duplicates(subset=['event_date'], keep='last', inplace=True)
+
+    # set event date as datetime index and resample to daily
+    # also drop the date column (it's the index now)
+    # and convert back to integers (resample changes dtype)
+    time_idx = pandas.DatetimeIndex(backlog.event_date)
+    backlog.set_index(time_idx, inplace=True)
+    backlog = backlog.resample('D').pad().drop(
+        columns='event_date'
+    ).convert_dtypes()
+
+    # limit our chart to dates after the birth of the backlog
+    backlog = backlog[
+        pandas.Timestamp(
+            '2015-10-09 00:00:00+0000', tz='UTC', freq='D'
+        ):pandas.Timestamp(
+            '2020-03-25 00:00:00+0000', tz='UTC', freq='D'
+        )
+    ]
+
+    # x data is time, y_data is our timeline values
+    x_data_c = backlog.c.tolist()
+    x_data_b = backlog.b.tolist()
+    x_data_s = backlog.s.tolist()
+    x_data_ns = backlog.ns.tolist()
+    y_data_dates = backlog.index.tolist()
+    # dates need to be converted to be JS-ready
+    y_data_dates = [
+        int(time_point.strftime("%s%f"))/1000 for time_point in y_data_dates
+    ]
+
+    # color data
+    area_colors = px.colors.sequential.Agsunset[::2]
+
+    return x_data_c, x_data_b, x_data_s, x_data_ns, y_data_dates, area_colors
