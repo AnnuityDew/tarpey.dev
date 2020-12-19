11ac8c273d1b60d3556f1de4425127e31142e1ec
AnnuityDew <annuitydew@gmail.com>
2020-12-18 23:43:41 -0500

    Auth is almost there...

--- api/users.py
+++ api/users.py
@@ -4,8 +4,10 @@ import json
 from typing import List
 
 # import third party packages
-from fastapi import APIRouter, Depends
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import (
+    OAuth2PasswordBearer, OAuth2PasswordRequestForm
+)
 import pandas
 import plotly
 import plotly.express as px
@@ -33,6 +35,7 @@ class ApprovedUsers(str, Enum):
 
 class UserBase(BaseModel):
     username: ApprovedUsers
+    disabled: Optional[bool] = None
 
     @validator('username')
     def username_alphanumeric(cls, name):
@@ -52,6 +55,61 @@ class UserDB(BaseModel):
     hashed_password: str
 
 
-@users_api.get('/test-token')
-async def read_items(token: str = Depends(oauth2_scheme)):
-    return {"token": token}
+@users_api.post("/token")
+async def login(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    client: MongoClient = Depends(get_dbm),
+    ):
+    db = client.users
+    collection = db.users
+
+    user_dict = collection.find_one({"_id": form_data.username})
+    if not user_dict:
+        raise HTTPException(
+            status_code=400,
+            detail="Incorrect username or password!",
+        )
+    # **user_dict passes in keys and values as model arguments
+    user = UserDB(**user_dict)
+
+    hashed_password = fake_hash_password(form_data.password)
+    if hashed_password != user.hashed_password:
+        raise HTTPException(
+            status_code=400,
+            detail="Incorrect username or password!",
+        )
+    
+    # OAuth2 spec
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
+@users_api.get("/me")
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(
+            status_code=400,
+            detail="Inactive user!"
+        )
+    return current_user
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    if not user:
+        # OAuth2 spec
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials!",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+def fake_decode_token(token):
+    return User(
+        username=token + "fakedecoded",
+    )
